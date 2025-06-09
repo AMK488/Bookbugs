47e5b62b128a288124cae720b76191cd39388664
big D <deepti.kalakoti@gmail.com>
2025-06-09 12:43:07 +0530

    Removed non-working imports and commented the code

--- BookstoreApp.java
+++ BookstoreApp.java
@@ -2,15 +2,10 @@
 
 import javax.swing.*;
 import java.awt.BorderLayout;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.awt.event.*;
 import java.util.*;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.GridLayout;
-
-
 
 
 // Book Class
@@ -91,7 +86,7 @@ class Admin extends User {
     }
 }
 
-// Transaction Class
+// Transaction Class(not functioning yet)
 class Transaction {
     private Date date;
     private Book book;
